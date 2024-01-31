7d3f1891911475fcf6dffeea92387f96c745a8de
tak <re795h@gmail.com>
2024-01-31 14:53:20 +0900

    deploy multisig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,24 @@
 import { HardhatUserConfig } from 'hardhat/types'
-
+import "hardhat-deploy";
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import 'dotenv/config'
+
+const DEPLOYER_KEY: string = process.env.DEPLOYER_KEY || process.env.MESSAGE_RELAYER__L1_WALLET || "";
 
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       blockGasLimit: 6133620
     },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: [DEPLOYER_KEY]
+    },
   },
+  namedAccounts: {
+		deployer: 0
+	},
   mocha: {
     timeout: 50000,
   },
